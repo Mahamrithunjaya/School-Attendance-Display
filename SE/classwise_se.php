aa9558337b3796427db13a0bff6a53159b6a81af
Mahamrithunjaya <sahasubhajitias59@gmail.com>
2024-11-09 00:16:06 +0530

    Classwise SE Data Page Updated

--- SE/classwise_se.php
+++ SE/classwise_se.php
@@ -6,6 +6,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="../img/logo.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="../img/logo.png" sizes="16x16">
     <title>Classwise Subject Enrichment</title>
     <link rel="stylesheet" href="se_classwise_styles.css">
 </head>
